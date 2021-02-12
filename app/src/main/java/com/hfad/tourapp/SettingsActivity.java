d104c551ee98bb2e42d6ef5eb4d0fcba676e8284
Joseph Barshay <62579564+gagusftw@users.noreply.github.com>
2021-02-12 17:30:04 -0500

    setting up preferences that persist

--- app/src/main/java/com/hfad/tourapp/SettingsActivity.java
+++ app/src/main/java/com/hfad/tourapp/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 
 
@@ -15,9 +16,10 @@ import androidx.appcompat.widget.SwitchCompat;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final String TAG = "SettingsActivity";
-    SwitchCompat swTTS;
-    SwitchCompat swNotify;
-    SwitchCompat swDarkMode;
+    SwitchCompat swMinimal; //Will only the location be announced or a summary from Wikipedia?
+    SwitchCompat swTTS; //Will the service do TTS announcement or just show current city?
+    SwitchCompat swNotify;  //Will notifications to announce when entering city be used?
+    SwitchCompat swDarkMode;    //Will dark mode be enabled?
     SharedPreferences prefs;
     SharedPreferences.Editor prefEditor;
 
@@ -31,12 +33,32 @@ public class SettingsActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
 
         //Initializing each of the switches
+        swMinimal = findViewById(R.id.swMinimal);
         swTTS = findViewById(R.id.swTTS);
         swNotify = findViewById(R.id.swNotify);
         swDarkMode = findViewById(R.id.swDarkMode);
-        prefs = this.getPreferences(Context.MODE_PRIVATE);
+        prefs = getSharedPreferences("com.hfad.tourapp.preferences", Context.MODE_PRIVATE);
+
+        if(prefs.getBoolean("minimal", false))
+            swMinimal.setChecked(true);
+        if(prefs.getBoolean("text-to-speech", false))
+            swTTS.setChecked(true);
+        if(prefs.getBoolean("notify", false))
+            swNotify.setChecked(true);
+        if(prefs.getBoolean("dark-mode", false))
+            swDarkMode.setChecked(true);
 
         //Setting up listeners for switches
+
+        swMinimal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.i(TAG, "swTTS listener");
+                prefEditor = prefs.edit();
+                prefEditor.putBoolean("minimal", isChecked);
+                prefEditor.apply();
+            }
+        });
 
         swTTS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
