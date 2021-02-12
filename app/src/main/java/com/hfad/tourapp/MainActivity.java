84cca4e421c8d141ea4c0e3dc3714c59282be191
Joseph Barshay <62579564+gagusftw@users.noreply.github.com>
2021-02-12 12:59:35 -0500

    Commit

--- app/src/main/java/com/hfad/tourapp/MainActivity.java
+++ app/src/main/java/com/hfad/tourapp/MainActivity.java
@@ -3,7 +3,11 @@ package com.hfad.tourapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +30,26 @@ public class MainActivity extends AppCompatActivity {
         txtCurrentCity = (TextView) findViewById(R.id.txtCurrentCity);
         // Set initial text for the current city indicator
         txtCurrentCity.setText(R.string.current_city_loading);
+    }
+
+    //Initialize ActionBar for MainActivity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
